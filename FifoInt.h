d9ca96e026dec9cdf40520a05e32845d4893f35a
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2018-07-24 17:21:08 +0300

    Update FifoInt.h

--- FifoInt.h
+++ FifoInt.h
@@ -2,24 +2,25 @@
 #define Fifo_h
 
 #include "Arduino.h"
+using type_fi = int;
 
 class Fifo {
 	public:
 		Fifo (int size);
-		Fifo (float *sample, int size);
-		void in (float val);
-		float out ();
-		float read ();
+		Fifo (type_fi *sample, int size);
+		void in (type_fi val);
+		type_fi out ();
+		type_fi read ();
 		int size ();
 		int count ();
 		bool isFull ();
 		bool isEmpty ();
 		bool allEqual ();
+        type_fi average ();
 	private:
 		int _size, _count, _inPtr, _outPtr;
-		float *_buf;
+		type_fi *_buf;
 		void increase (int *ptr);
 };
-
 
 #endif
